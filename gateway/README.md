5dc9287dacb5d6635bd73ee6bd9244cfbfe41f49
Brendan Keane <btkeane@gmail.com>
2022-05-07 23:31:01 -0700

    adding basic gateway proxy for local execution

---
+++ gateway/README.md
@@ -0,0 +1,5 @@
+# Instructions
+1. `pip install -r requirements.txt`
+1. export `LAMBDA_ENDPOINT` (ex: `http://localhost:8080`)
+1. `python gateway.py`
+1. visit `http://localhost:8081`
---
+++ gateway/models/api_gateway_v2.py
@@ -0,0 +1,60 @@
+from pydantic import BaseModel, ValidationError, validator
+from typing import Dict, List, Optional
+from humps import camelize
+
+def to_camel(string):
+    return camelize(string)
+
+class CamelModel(BaseModel):
+    class Config:
+        alias_generator = to_camel  
+        allow_population_by_field_name = True
+
+class HttpStruct(CamelModel):
+    method: str
+    path: str
+    protocol: str = "HTTP/1.1"
+    source_ip: Optional[str]
+    user_agent: Optional[str]
+
+    @validator("method")
+    def validate_method(cls, v):
+        valid_methods = ["DELETE", "GET", "HEAD", "OPTIONS", "PATCH", "POST", "PUT"]
+        if v in valid_methods:
+            return v
+        else:
+            raise ValidationError(f"{v} not a valid HTTP verb")
+
+
+class RequestContextStruct(CamelModel):
+    account_id: Optional[str]
+    authentication: Optional[str]
+    authorizer: Optional[Dict]
+    domain_name: Optional[str]
+    domain_prefix: str = "id"
+    http: HttpStruct
+    request_id: Optional[str]
+    route_key: str = "$default"
+    stage: str = "$default"
+    time: Optional[str]
+    time_epoch: Optional[int]
+
+class APIGatewayV2Event(CamelModel):
+    version: str = "2.0"
+    route_key: str = "$default"
+    raw_path: str
+    raw_query_string: Optional[str]
+    cookies: List[str]
+    headers: Dict[str, str]
+    query_string_parameters: Optional[Dict[str, str]]
+    request_context: RequestContextStruct
+    body: Optional[str]
+    path_parameters: Optional[Dict[str, str]]   
+    is_base_64_encoded: bool = False
+    stage_variables: Optional[Dict[str, str]]
+
+class APIGatewayV2Response(CamelModel):
+    status_code: int
+    body: str
+    headers: Dict
+    is_base_64_encoded: bool
