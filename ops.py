0f0e0dc999847d13e2d2b2c4fbfa62351e7c52a1
Raymond Douglas <r@rymnd.org>
2022-05-21 13:01:54 -0700

    restructure, add error handling, registry commands

--- ops.py
+++ ops.py
@@ -1,61 +1,16 @@
 #!/usr/bin/env python3
 
-import json
-import boto3
 import typer
 from typing import Optional
-from aws_lambda_powertools.utilities import parameters
-from pathlib import Path
 
-
-class Parameters:
-    def __init__(self, kms_key_alias, prefix):
-        # SSM
-        self.prefix = str(Path(f"/{prefix}"))
-        self.ssm_client = boto3.client("ssm")
-        self.ssm_provider = parameters.SSMProvider()
-
-        # KMS
-        self.kms_key_alias = f"alias/{kms_key_alias}"
-        self.kms_client = boto3.client("kms")
-
-    def kms_key_id(self):
-        kms_key_id = None
-        for alias in self.kms_client.list_aliases()["Aliases"]:
-            if alias["AliasName"] == self.kms_key_alias:
-                kms_key_id = alias["TargetKeyId"]
-        return kms_key_id
-
-    def set(self, key, value, description="created by ops.py"):
-        name = str(Path(f"{self.prefix}/{key}"))
-        self.ssm_client.put_parameter(
-            Name=name,
-            Description=description,
-            Value=value,
-            Type="SecureString",
-            KeyId=self.kms_key_id(),
-            Overwrite=True,
-            DataType="text",
-        )
-
-    def get(self, filter="", decrypt=True):
-        results = self.ssm_provider.get_multiple(
-            path=self.prefix, decrypt=decrypt, recursive=True
-        )
-        filtered = {}
-        for key, value in results.items():
-            if key.startswith(filter):
-                filtered[key] = value
-        return filtered
-
-    def delete(self, key):
-        name = str(Path(f"{self.prefix}/{key}"))
-        self.ssm_client.delete_parameter(Name=name)
+from ops.lib import Parameters, Registry
 
 
 cli = typer.Typer()
 params = typer.Typer()
+registry = typer.Typer()
 cli.add_typer(params, name="params", help="manage SSM parameters")
+cli.add_typer(registry, name="registry", help="manage image registry")
 
 
 @cli.command()
@@ -70,9 +25,9 @@ def init(
     api_description = typer.prompt("Description")
     api_repository = typer.prompt("Repository")
 
-    Parameters(kms_key_alias, prefix).set("name", api_name)
-    Parameters(kms_key_alias, prefix).set("description", api_description)
-    Parameters(kms_key_alias, prefix).set("repository", api_repository)
+    typer.echo(Parameters(kms_key_alias, prefix).set("name", api_name))
+    typer.echo(Parameters(kms_key_alias, prefix).set("description", api_description))
+    typer.echo(Parameters(kms_key_alias, prefix).set("repository", api_repository))
 
     typer.echo(typer.style(f"{api_name} initialized", fg=typer.colors.GREEN))
 
@@ -87,20 +42,19 @@ def set(
     """
     set a parameter in SSM
     """
-    Parameters(kms_key_alias, f"{prefix}/runtime/").set(key, value)
-
-    typer.echo(typer.style(f"{prefix}/runtime/{key} == {value}", fg=typer.colors.GREEN))
+    typer.echo(Parameters(kms_key_alias, f"{prefix}/runtime/").set(key, value))
 
 
 @params.command()
 def get(
+    filter: str = typer.Argument(""),
     kms_key_alias: str = typer.Option("aws/ssm", envvar="KMS_KEY_ALIAS"),
     prefix: str = typer.Option(..., envvar="PREFIX"),
 ):
     """
     read parameters from SSM
     """
-    typer.echo(json.dumps(Parameters(kms_key_alias, prefix).get(), indent=2))
+    typer.echo(Parameters(kms_key_alias, prefix).get(filter))
 
 
 @params.command()
@@ -112,9 +66,40 @@ def delete(
     """
     delete a parameter from SSM
     """
-    Parameters(kms_key_alias, f"{prefix}/runtime/").delete(key)
+    typer.echo(Parameters(kms_key_alias, f"{prefix}/runtime/").delete(key))
 
-    typer.echo(typer.style(f"{prefix}/runtime/{key} deleted", fg=typer.colors.GREEN))
+
+@registry.command()
+def list(
+    kms_key_alias: str = typer.Option("aws/ssm", envvar="KMS_KEY_ALIAS"),
+    prefix: str = typer.Option(..., envvar="PREFIX"),
+):
+    """
+    list images in registry
+    """
+    results = Parameters(kms_key_alias, prefix).get("name")
+    for key, value in results.items():
+        if key == "name":
+            typer.echo(typer.style(f"{value} versions:", fg=typer.colors.GREEN))
+            for version in Registry(value).list():
+                typer.echo(version["pushed"])
+                for tag in version["tags"]:
+                    typer.echo(typer.style(f"   {tag}", fg=typer.colors.BLUE))
+
+
+@registry.command()
+def delete(
+    tag: str,
+    kms_key_alias: str = typer.Option("aws/ssm", envvar="KMS_KEY_ALIAS"),
+    prefix: str = typer.Option(..., envvar="PREFIX"),
+):
+    """
+    delete image(s) in registry
+    """
+    results = Parameters(kms_key_alias, prefix).get("name")
+    for key, value in results.items():
+        if key == "name":
+            typer.echo(Registry(value).delete(tag))
 
 
 if __name__ == "__main__":
---
+++ ops/lib.py
@@ -0,0 +1,124 @@
+import sys
+import json
+import boto3
+from typer import echo, style, colors
+from pathlib import Path
+from botocore.exceptions import ClientError
+from aws_lambda_powertools.utilities import parameters
+
+# Validate AWS credentials
+sts = boto3.client("sts")
+try:
+    sts.get_caller_identity()
+except ClientError as e:
+    if e.response["Error"]["Code"] == "InvalidClientTokenId":
+        echo(style("Invalid AWS credentials", fg=colors.RED))
+        sys.exit(1)
+    else:
+        echo(style(f"Identity error: {e}", fg=colors.RED))
+        sys.exit(1)
+
+
+class Parameters:
+    def __init__(self, kms_key_alias, prefix):
+        # SSM
+        self.prefix = str(Path(f"/{prefix}"))
+        self.ssm_client = boto3.client("ssm")
+        self.ssm_provider = parameters.SSMProvider()
+
+        # KMS
+        self.kms_key_alias = f"alias/{kms_key_alias}"
+        self.kms_client = boto3.client("kms")
+
+    def kms_key_id(self):
+        kms_key_id = None
+        for alias in self.kms_client.list_aliases()["Aliases"]:
+            if alias["AliasName"] == self.kms_key_alias:
+                kms_key_id = alias["TargetKeyId"]
+        return kms_key_id
+
+    def set(self, key, value, description="created by ops.py"):
+        name = str(Path(f"{self.prefix}/{key}"))
+        try:
+            self.ssm_client.put_parameter(
+                Name=name,
+                Description=description,
+                Value=value,
+                Type="SecureString",
+                KeyId=self.kms_key_id(),
+                Overwrite=True,
+                DataType="text",
+            )
+            return style(f"{self.prefix}/{key}: {value}", fg=colors.GREEN)
+        except ClientError as e:
+            return style(f"{self.prefix}/{key}: {e}", fg=colors.RED)
+
+    def get(self, filter, decrypt=True):
+        try:
+            results = self.ssm_provider.get_multiple(
+                path=self.prefix, decrypt=decrypt, recursive=True
+            )
+        except parameters.exceptions.GetParameterError as e:
+            return style(f"Parameter error: {e}", fg=colors.RED)
+        except ClientError as e:
+            return style(f"Client error: {e}", fg=colors.RED)
+
+        filtered = {}
+        for key, value in results.items():
+            if key.startswith(filter):
+                filtered[key] = value
+        return filtered
+
+    def delete(self, key):
+        name = str(Path(f"{self.prefix}/{key}"))
+        try:
+            self.ssm_client.delete_parameter(Name=name)
+            return style(f"{self.prefix}/{key}: deleted", fg=colors.GREEN)
+        except ClientError as e:
+            if e.response["Error"]["Code"] == "ParameterNotFound":
+                return style(f"{self.prefix}/{key}: parameter not found", fg=colors.RED)
+            else:
+                return style(f"{self.prefix}/{key}: {e}", fg=colors.RED)
+
+
+class Registry:
+    def __init__(self, name):
+        # ECR
+        self.name = name
+        self.ecr_client = boto3.client("ecr")
+
+    def list(self):
+        try:
+            results = self.ecr_client.describe_images(repositoryName=self.name)[
+                "imageDetails"
+            ]
+        except ClientError as e:
+            if e.response["Error"]["Code"] == "RepositoryNotFoundException":
+                return style(f"{self.name}: repository not found", fg=colors.RED)
+            else:
+                return style(f"Client error: {e}", fg=colors.RED)
+        images = []
+        for image in results:
+            # Could probably utilize a dataclass
+            version = {
+                "pushed": f"{image['imagePushedAt']}",
+                "tags": image["imageTags"],
+            }
+            images.append(version)
+
+        return images
+
+    def delete(self, tag):
+        try:
+            self.ecr_client.describe_images(
+                repositoryName=self.name, imageIds=[{"imageTag": tag}]
+            )
+            self.ecr_client.batch_delete_image(
+                repositoryName=self.name, imageIds=[{"imageTag": tag}]
+            )
+            return style(f"{tag}: deleted", fg=colors.GREEN)
+        except ClientError as e:
+            if e.response["Error"]["Code"] == "ImageNotFoundException":
+                return style(f"{self.name}:{tag}: image not found", fg=colors.RED)
+            else:
+                return style(f"Client error: {e}", fg=colors.RED)
