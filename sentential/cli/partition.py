979497add6f61aac649e66d41ed515ad9713f9a2
Brendan Keane <btkeane@gmail.com>
2022-07-30 12:39:30 -0700

    stop abusing pydantic model for facts, start using object with lazy eval'd properties

---
+++ sentential/cli/partition.py
@@ -0,0 +1,12 @@
+import typer
+
+aws = typer.Typer()
+
+
+@aws.command()
+def add(partition: str):
+    pass
+
+@aws.command()
+def remove(partition: str):
+    pass
--- sentential/lib/facts.py
+++ sentential/lib/facts.py
@@ -1,48 +1,38 @@
 import boto3
 from yaml import safe_load
 from pathlib import Path
-from pydantic import BaseModel, validator
-from typing import Optional, Any
 from sentential.lib.clients import clients
 from sentential.lib.shapes.internal import Paths
+from types import SimpleNamespace
+
+SNTL_FILE = SimpleNamespace(**safe_load(open("./.sntl/sentential.yml")))
+
+def lazy_property(fn):
+    '''Decorator that makes a property lazy-evaluated.'''
+    attr_name = '_lazy_' + fn.__name__
+
+    @property
+    def _lazy_property(self):
+        if not hasattr(self, attr_name):
+            setattr(self, attr_name, fn(self))
+        return getattr(self, attr_name)
+    return _lazy_property
 
 
-def find_sentential():
-    try:
-        return safe_load(open("./.sntl/sentential.yml"))["repository_name"]
-    except:
-        return None
+class Facts:
+    """Most properties in this object are lazy loaded, don't get the data if the data isn't needed"""
+    def __init__(self, repository_name: str = SNTL_FILE.repository_name , runtime: str = None, kms_key_alias: str = "aws/ssm") -> None:
+        self.repository_name = repository_name
+        self.runtime = runtime
+        self.kms_key_alias = kms_key_alias
 
-
-class Facts(BaseModel):
-    repository_name: str = find_sentential()
-    runtime: Optional[str]
-    region: str = boto3.session.Session().region_name
-    path: Optional[Paths]
-    account_id: str = clients.sts.get_caller_identity().get("Account")
-    kms_key_alias: str = "aws/ssm"
-    kms_key_id: Optional[str]
-    repository_url: Optional[str]
-    registry_url: Optional[str]
-
-    @validator("kms_key_id", always=True)
-    def lookup_kms_key_id(cls, v, values) -> str:
-        return [
-            ssm_key["TargetKeyId"]
-            for ssm_key in boto3.client("kms").list_aliases()["Aliases"]
-            if values["kms_key_alias"] in ssm_key["AliasName"]
-        ][0]
-
-    @validator("repository_url", always=True)
-    def assemble_repository_url(cls, v, values) -> str:
-        return f"{values['account_id']}.dkr.ecr.{values['region']}.amazonaws.com/{values['repository_name']}"
-
-    @validator("registry_url", always=True)
-    def assemble_registry_url(cls, v, values) -> str:
-        return f"{values['account_id']}.dkr.ecr.{values['region']}.amazonaws.com"
-
-    @validator("path", always=True)
-    def assemble_path(cls, v, values) -> str:
+    @lazy_property
+    def region(self):
+        print("this should not have happened")
+        return boto3.session.Session().region_name
+    
+    @lazy_property
+    def path(self):
         root = Path(".")
         return Paths(
             root=root,
@@ -54,5 +44,24 @@ class Facts(BaseModel):
             policy=Path(f"{root}/policy.json"),
         )
 
+    @lazy_property
+    def account_id(self):
+        return clients.sts.get_caller_identity().get("Account")
+
+    @lazy_property
+    def kms_key_is(self):
+        return [
+            ssm_key["TargetKeyId"]
+            for ssm_key in boto3.client("kms").list_aliases()["Aliases"]
+            if self.kms_key_alias in ssm_key["AliasName"]
+        ][0]
+
+    @lazy_property
+    def repository_url(self):
+        return f"{self.account_id}.dkr.ecr.{self.region}.amazonaws.com/{self.repository_name}"
+
+    @lazy_property
+    def registry_url(self):
+        return f"{self.account_id}.dkr.ecr.{self.region}.amazonaws.com"
 
 facts = Facts()
