bd90b3a29c12c7cf0001d589766ae3cf146a1152
Brendan Keane <btkeane@gmail.com>
2022-09-01 16:41:41 -0700

    fixing ls, same problem, different method

--- sentential/lib/ontology.py
+++ sentential/lib/ontology.py
@@ -2,6 +2,7 @@ from sentential.lib.const import SEMVER_REGEX
 from sentential.lib.aws import Repository as AwsRepository
 from sentential.lib.local import Repository as LocalRepository
 from distutils.version import StrictVersion
+from typing import List
 import semantic_version as semver
 from rich.table import Table
 from rich import print
@@ -16,7 +17,7 @@ class Ontology:
         self.rgx = re.compile(SEMVER_REGEX)
         self.df = self.local.join(self.aws, on="Sha", how="outer")
 
-    def _extract(self):
+    def _extract(self) -> pl.DataFrame:
         return self.df.select(
             [
                 pl.col("Sha").alias("sha"),
@@ -26,7 +27,7 @@ class Ontology:
             ]
         )
 
-    def _sort(self, df):
+    def _sort(self, df) -> pl.DataFrame:
         table = self._extract().drop_nulls("tag").drop_nulls("tag")
         rows = [row for row in table.rows() if self.rgx.match(row[1])]
         rows.sort(key=lambda row: StrictVersion(row[1]))
@@ -36,45 +37,50 @@ class Ontology:
         tag = [row[1] for row in rows]
         deployed_local = [row[2] for row in rows]
         deployed_aws = [row[3] for row in rows]
-
         return pl.DataFrame(
             [sha, tag, deployed_local, deployed_aws], columns=table.columns
         )
 
-    def _squash(self, df):
+    def _squash(self, df) -> pl.DataFrame:
         table = df.groupby("sha").agg_list()
         rows = table.to_dicts()
         for row in rows:
             row["tag"] = list(set(row["tag"]))
             row["deployed_local"] = any(row["deployed_local"])
             row["deployed_aws"] = any(row["deployed_aws"])
+        # TODO: this is pretty lame, do it another way.
+        sha = [ row['sha'] for row in rows ]
+        tag = [ row['tag'] for row in rows ]
+        deployed_local = [ row['deployed_local'] for row in rows ]
+        deployed_aws = [ row['deployed_aws'] for row in rows ]
+        return pl.DataFrame(
+            [sha, tag, deployed_local, deployed_aws], columns=table.columns
+        )
 
-        return pl.DataFrame([list(row.values()) for row in rows], columns=table.columns)
-
-    def latest_semver(self):
+    def latest_semver(self) -> str:
         try:
             latest = list(self._sort(self._extract()).get_column("tag"))[0]
         except IndexError:
             latest = None
         return latest
 
-    def next_build_semver(self):
+    def next_build_semver(self) -> str:
         latest = self.latest_semver()
         if latest is None:
             latest = "0.0.0"
         return semver.Version(latest).next_patch()
 
-    def semvers(self):
+    def semvers(self) -> List[str]:
         return list(self._sort(self._extract()).get_column("tag"))
 
-    def sha_exists(self, sha):
+    def sha_exists(self, sha) -> bool:
         table = self._squash(self._extract())
         for row in table.to_dicts():
             if row["sha"] == sha:
                 return True
         return False
 
-    def print(self):
+    def print(self) -> Table:
         df = self._squash(self._sort(self._extract()))
         table = Table(*df.columns)
         for row in df.rows():
