173489b9870f2320eed7b3ac3e1cbd19f5b43edd
Raymond Douglas <r@rymnd.org>
2022-09-03 12:34:03 -0700

    formatting

--- sentential/templates/shapes.py
+++ sentential/templates/shapes.py
@@ -1,16 +1,16 @@
 from sentential.support.shaper import Shaper, List, Field
 
 """
-# Define, describe, and validate env's and arg's
+# Define, describe, and validate environment variables and build arguments.
 
-# See documentation for general usage
+# See documentation for general usage.
 
-# example:
- class Env(Shaper):
-     db_url: str
-     db_port: int = 3306
-     db_username: str = "postgres"
-     db_password: str
+# Example:
+class Env(Shaper):
+    db_url: str
+    db_port: int = 3306
+    db_username: str = "postgres"
+    db_password: str
 
 #
 # Compatible Classes
