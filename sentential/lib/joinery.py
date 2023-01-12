b0aa332d270598e8e9315823e9219745b745d1ea
Brendan Keane <btkeane@gmail.com>
2023-01-11 21:09:17 -0800

    code bad, concept good?

--- sentential/lib/joinery.py
+++ sentential/lib/joinery.py
@@ -2,7 +2,6 @@ from distutils.version import LooseVersion
 from typing import Any, List, Union
 from rich.table import Table, box
 from sentential.lib.clients import clients
-from sentential.lib.drivers.local_bridge import LocalBridge
 from sentential.lib.ontology import Ontology
 from sentential.lib.shapes import (
     AwsManifestList,
@@ -31,7 +30,7 @@ class Joinery:
         url = f"https://{region}.console.aws.amazon.com/lambda/home?region={region}#/functions/{function}"
         return self._to_hyperlink(url, "console")
 
-    def _get_aws_rows(self) -> List[List[str]]:
+    def _get_aws_rows(self, cwi_digest: str = "") -> List[List[str]]:
         deployed = self.aws_lambda.deployed_function()
         public_url = self.aws_lambda.deployed_public_url()
 
@@ -50,23 +49,27 @@ class Joinery:
                 ]
             )
 
-            manifest_list_digest = manifest.imageId.imageDigest.replace("sha256:", "")[
-                0:12
-            ]
-
-            image_digests = [dist.digest for dist in manifest.imageManifest.manifests]
+            manifest_list_digest = manifest.imageId.imageDigest.replace("sha256:", "")[0:12]
+            image_digests = [dist.digest.replace("sha256:","")[0:12] for dist in manifest.imageManifest.manifests]
+            
             hrefs = []
-            if deployed and f"sha256:{deployed.CodeSha256}" in image_digests:
+            status = ""
+            cwi = "false"
+            if deployed and deployed.CodeSha256[0:12] in image_digests:
                 status = deployed.State.lower()
                 hrefs = [self._webconsole_hyperlink()]
                 if public_url:
                     hrefs.append(
                         self._to_hyperlink(public_url.FunctionUrl, "public_url")
-                    )
-            else:
-                status = ""
+                    )                
 
-            rows.append([tag, arch, manifest_list_digest, status, ", ".join(hrefs)])
+                if cwi_digest:
+                    if cwi_digest == manifest_list_digest:
+                        cwi = "true"
+                    if cwi_digest in image_digests:
+                        cwi = "true"
+
+            rows.append([cwi, tag, arch, manifest_list_digest, status, ", ".join(hrefs)])
 
         return sorted(rows, key=lambda row: LooseVersion(row[0]), reverse=True)
 
@@ -85,7 +88,7 @@ class Joinery:
             hrefs = ""
 
             if cwi.repo_digests:
-                digest = cwi.repo_digests[0].split("@")[-1].replace("sha256:", "")[0:12]
+                digest = cwi.repo_digests[0].split("@")[-1].replace("sha256:","")[0:12]
 
             if cwc and cwc.image == cwi.id:
                 if cwc.state.status:
@@ -94,30 +97,30 @@ class Joinery:
             if url:
                 hrefs = self._to_hyperlink(url, "public_url")
 
-            return ["local", arch, digest, status, hrefs]
+            return ["true", "local", arch, digest, status, hrefs]
 
         except LocalDriverError:
             return None
 
     def _highlight(self, rows):
-        if rows and rows[0][0] == "local" and rows[0][2]:
-            digest = rows[0][2]
-            for i, row in enumerate(rows):
-                if row[2] == digest:
-                    rows[i][2] = f"[yellow]{row[2]}[/yellow]"
+        for i, row in enumerate(rows):
+            if row[0] == "true":
+                rows[i][1] = f"[yellow]{rows[i][1]}[/yellow]"
 
     def list(self) -> Table:
         columns = ["build", "arch", "digest", "status", "hrefs"]
         table = Table(box=box.SIMPLE, *columns)
-        rows = self._get_aws_rows()
         cwi_row = self._get_cwi_row()
 
         if cwi_row:
+            rows = self._get_aws_rows(cwi_row[3])
             rows.insert(0, cwi_row)
-
+        else:
+            rows = self._get_aws_rows()
+            
         self._highlight(rows)
 
         for row in rows:
-            table.add_row(*row)
+            table.add_row(*row[1:])
 
         return table
