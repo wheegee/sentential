0ba03093d392fffb607359c851aa5a4174f5a1c3
Raymond Douglas <r@rymnd.org>
2023-03-09 11:44:21 -0800

    add test for schedule unmounting

--- tests/lib/mounts/test_aws_event_schedule.py
+++ tests/lib/mounts/test_aws_event_schedule.py
@@ -3,8 +3,8 @@ from shutil import copyfile
 from sentential.lib.shapes import Architecture
 from sentential.lib.drivers.aws_lambda import AwsLambdaDriver
 from sentential.lib.drivers.aws_ecr import AwsEcrDriver
-from sentential.lib.exceptions import AWS_EXCEPTIONS
 from sentential.lib.mounts.aws_event_schedule import AwsEventScheduleMount
+from sentential.lib.exceptions import AWS_EXCEPTIONS
 from sentential.lib.clients import clients
 
 
@@ -19,24 +19,35 @@ class TestAwsEventScheduleMount:
         return clients.ebr.list_targets_by_rule(Rule=rule_name)
 
     def test_mount(
-        self, aws_lambda_driver: AwsLambdaDriver, aws_ecr_driver: AwsEcrDriver
+        self,
+        aws_lambda_driver: AwsLambdaDriver,
+        aws_ecr_driver: AwsEcrDriver,
     ):
-        cron_expression = "cron(0 20 * * ? *)"
-        payload = '{"foo": "bar"}'
         image = aws_ecr_driver.get_image()
         aws_lambda_driver.deploy(image, Architecture.system())
+        function_name = aws_lambda_driver.ontology.context.resource_name
+        function_arn = aws_lambda_driver.ontology.context.resource_arn
+
+        cron_expression = "cron(0 20 * * ? *)"
+        payload = '{"foo": "bar"}'
         AwsEventScheduleMount(aws_lambda_driver.ontology).mount(
             cron_expression, payload
         )
-        schedule_expression = self.get_rule(
-            aws_lambda_driver.ontology.context.resource_name
-        )["ScheduleExpression"]
-        schedule_targets = self.get_targets(
-            aws_lambda_driver.ontology.context.resource_name
-        )["Targets"]
+
+        schedule_expression = self.get_rule(function_name)["ScheduleExpression"]
+        schedule_targets = self.get_targets(function_name)["Targets"]
+
         assert cron_expression == schedule_expression
-        assert (
-            aws_lambda_driver.ontology.context.resource_arn
-            == schedule_targets[0]["Arn"]
-        )
+        assert function_arn == schedule_targets[0]["Arn"]
         assert payload == schedule_targets[0]["Input"]
+
+    def test_umount(
+        self,
+        aws_lambda_driver: AwsLambdaDriver,
+    ):
+        function_name = aws_lambda_driver.ontology.context.resource_name
+
+        AwsEventScheduleMount(aws_lambda_driver.ontology).umount()
+
+        with pytest.raises(AWS_EXCEPTIONS):  # this is a tad lame
+            self.get_rule(function_name)
