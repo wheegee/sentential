42e0c396401196d3cfcb59fde396470b5fd4e6b8
Raymond Douglas <r@rymnd.org>
2023-04-12 13:24:09 -0700

    correct global constants

--- sentential/lib/shapes.py
+++ sentential/lib/shapes.py
@@ -14,8 +14,8 @@ from sentential.lib.clients import clients
 #
 
 SNTL_WORKING_IMAGE_TAG = os.getenv("SNTL_WORKING_IMAGE_TAG", default=("cwi"))
-SNTL_ENTRY_VERSION = os.getenv("SNTL_ENTRY_VERSION", default=("0.4.1"))
-SNTL_ENTRY_PATHS = os.getenv("SNTL_ENTRY_PATHS", default=("${SSM_PATHS}"))
+SNTL_ENTRY_VERSION = os.getenv("SNTL_ENTRY_VERSION", default=("0.4.2"))
+SNTL_ENTRY_PATHS = os.getenv("SNTL_ENTRY_PATHS", default=("${PARTITION}/Envs,${PARTITION}/Secrets"))
 
 #
 # Store
